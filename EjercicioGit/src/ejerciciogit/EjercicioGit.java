4c4e296349653392455785506fe41020ab21beef
dam <dam@203P20>
2022-01-19 09:55:01 +0100

    modificar archivos rama nueva

--- EjercicioGit/src/ejerciciogit/EjercicioGit.java
+++ EjercicioGit/src/ejerciciogit/EjercicioGit.java
@@ -17,6 +17,7 @@ public class EjercicioGit {
     public static void main(String[] args) {
         System.out.println("Hola");
         System.out.println("Buenos dias");
+        System.out.println("Esta en la rama nueva.");
     }
     
 }
